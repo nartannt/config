6db7ba518d2c078a467b1461addd63a732088d40
nartan <bozec.tanguy@gmail.com>
2025-11-17 14:08:02 +0100

    added locale settings for tulkas

--- hosts/tulkas/default.nix
+++ hosts/tulkas/default.nix
@@ -29,6 +29,20 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
